4c8062d8fe24bebe2a1f2a136f3bfb212f79cb74
SRISAIFINANCIAL <srisaifinancialservices22@gmail.com>
2025-12-27 20:29:36 +0530

    Update server.js

--- server.js
+++ server.js
@@ -16,7 +16,8 @@ app.use(
     origin: [
      "http://localhost:8080",
       "https://srisaifinanceadmin.vercel.app",
-      "https://srisaifinance.vercel.app"
+      "https://srisaifinance.vercel.app",
+      "'https://srisaifinance.vercel.app"
     ],
     methods: ["GET", "POST", "PATCH", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
